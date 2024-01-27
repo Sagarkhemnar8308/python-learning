c0d1b22167d42e112b589f0106aa42d0fa66f748
sagar81 <sagar_81>
2024-01-27 08:04:35 +0530

    day5

---
+++ day5/encode.py
@@ -0,0 +1,15 @@
+def fun(a):
+    
+ if a=="!":
+     print(f"Exlametery")   
+ elif a=='@':
+     print(f"mention")
+ elif a=='$':
+     print(f"Dolar")
+ else:
+    print(f" you enter is not a special character")
+     
+b=input("Enter a Special character")
+
+fun(b)
+ 
